008be4d0745afe375e0ab3b5571a00868161945f
SD44 <SD44@mitt.ca>
2018-12-20 12:30:06 -0600

    Add the map access key

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoiYmxha2VicjAiLCJhIjoiY2pwd3RlNTExMGJjYzN5bXY4dWQ4c3E2aiJ9.sqJ0TUODKUfB41cJI9qQLg',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
